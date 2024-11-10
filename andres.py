0aff1b0cb8f37eaba50f96fea32fda52007df1ca
Andres Roche <hewsanchez@gmail.com>
2024-11-10 17:39:05 -0400

    Andres first commit

---
+++ andres.py
@@ -0,0 +1,8 @@
+import time
+
+if __name__=="__main__":
+	print("Hola Mundo, soy andres")
+	print("quieres un chiste?")
+	print("porque los a los papas les encanta el mini golf?")
+	time.sleep(2)
+	print("porque tiene menos de 18 hoyos....")
