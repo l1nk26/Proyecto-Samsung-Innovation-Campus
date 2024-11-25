7c403934ef32a2a01d09bdd91d65b96767ab2472
Andres Roche <hewsanchez@gmail.com>
2024-11-25 10:42:46 -0400

    medio analisis andres

--- andres.py
+++
@@ -1,8 +0,0 @@
-import time
-
-if __name__=="__main__":
-	print("Hola Mundo, soy andres")
-	print("quieres un chiste?")
-	print("porque los a los papas les encanta el mini golf?")
-	time.sleep(2)
-	print("porque tiene menos de 18 hoyos....")
